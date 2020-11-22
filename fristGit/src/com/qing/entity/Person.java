bc9c4edccc72a01a605a571704f9e6669ef11788
qingge <57626278@qq.com>
2020-11-22 16:57:13 +0800

    person类

---
+++ fristGit/src/com/qing/entity/Person.java
@@ -0,0 +1,13 @@
+package com.qing.entity;
+
+/**
+ * @Package: com.qing.entity
+ * @ClassName: Person
+ * @Author: QingGe
+ * @CreateTime: 2020/11/22 16:56
+ * @Description:
+ */
+public class Person {
+    private int age;
+
+}
